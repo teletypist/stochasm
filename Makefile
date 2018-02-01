29f9556658add74fced88bedb077e778c63b3c5d
Frederick Rainbird <frainbird@gail.com>
2018-02-01 13:12:23 +1100

    Created Stochasm and Resonator plugin

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-SLUG = Template
+SLUG = com-soundchasing-stochasm
 VERSION = 0.5.0
 
 # FLAGS will be passed to both the C and C++ compiler
--- src/Stochasm.cpp
+++ src/Stochasm.cpp
@@ -1,4 +1,4 @@
-#include "Template.hpp"
+#include "Stochasm.hpp"
 
 
 // The plugin-wide instance of the Plugin class
@@ -12,11 +12,11 @@ void init(rack::Plugin *p) {
 	// The ZIP package must only contain one folder, with the name equal to the plugin's slug.
 	p->slug = TOSTRING(SLUG);
 	p->version = TOSTRING(VERSION);
-	p->website = "https://github.com/VCVRack/Template";
-	p->manual = "https://github.com/VCVRack/Template/blob/master/README.md";
+	p->website = "https://soundchasing.com/uncertainty";
+	p->manual = "https://soundchasing.com/stochasm";
 
 	// For each module, specify the ModuleWidget subclass, manufacturer slug (for saving in patches), manufacturer human-readable name, module slug, and module name
-	p->addModel(createModel<MyModuleWidget>("Template", "MyModule", "My Module", OSCILLATOR_TAG));
+	p->addModel(createModel<ResonatorWidget>("Stochasm", "Resonator", "Bitstream Resonator", OSCILLATOR_TAG));
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
